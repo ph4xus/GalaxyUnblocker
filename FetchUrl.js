5b1f6b48373774a7cf9f32dc3ee15e57e4c5943a
ph4xus <168146698+ph4xus@users.noreply.github.com>
2025-01-14 13:09:33 -0700

    Update FetchUrl.js

--- FetchUrl.js
+++ FetchUrl.js
@@ -27,7 +27,7 @@ function spam() {
         //const words = ["island boys kissing","isis application form","penis enlargment pills","pornhub gay","free v bucks generators","why is age of consent so old here","free gay porn","little boys kissing","gay hentai","massive gay orgy","fortnite sus emotes to do","thicc chun li hips","chun li doing party hips 10 hours loop"]
         let google = "https://www.google.com/search?surl=1&sca_esv=3ec098e3b7112be9&rlz=1CAKLUN_enUS1139&q=" +  words[Math.floor(Math.random() * words.length)] + "&udm=2"
         open(google, 'test', params);
-      setTimeout(loop, 150);
+      setTimeout(loop, 100);
     }
   }
 
