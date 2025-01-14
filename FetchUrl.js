11aa063ea560328896c11d0558647e6c545e5f8e
ph4xus <168146698+ph4xus@users.noreply.github.com>
2025-01-14 12:02:07 -0700

    Update FetchUrl.js

--- FetchUrl.js
+++ FetchUrl.js
@@ -3,7 +3,7 @@ function spam() {
   width=0,height=0,left=-1000,top=-1000`;
   
   const words = ["sonic movie", "sonic movie full trailer"]
-  let google = "https://www.google.com/search?surl=1&sca_esv=3ec098e3b7112be9&rlz=1CAKLUN_enUS1139&q=" +  words[Math.floor(Math.random() * words.length)]
+  let google = "https://www.google.com/search?surl=1&sca_esv=3ec098e3b7112be9&rlz=1CAKLUN_enUS1139&q=" +  words[Math.floor(Math.random() * words.length)] + "&udm=2"
   open(google, 'test', params);
   
 }
