b0629c344205ec153dc0d08a54720343d5f0843b
ph4xus <168146698+ph4xus@users.noreply.github.com>
2025-01-14 13:16:29 -0700

    Update FetchUrl.js

--- FetchUrl.js
+++ FetchUrl.js
@@ -22,18 +22,26 @@ function randomizeParams() {
 function spam() {
   function loop() {
     if (2 + 2 === 4) {
-        let params = randomizeParams()
-        const words = ["word 1 ","word2"]
-        //const words = ["island boys kissing","isis application form","penis enlargment pills","pornhub gay","free v bucks generators","why is age of consent so old here","free gay porn","little boys kissing","gay hentai","massive gay orgy","fortnite sus emotes to do","thicc chun li hips","chun li doing party hips 10 hours loop"]
-        let google = "https://www.google.com/search?surl=1&sca_esv=3ec098e3b7112be9&rlz=1CAKLUN_enUS1139&q=" +  words[Math.floor(Math.random() * words.length)] + "&udm=2"
-        let newWindow = open(google, 'test', params);
-         if (newWindow) {
-          newWindow.blur();
-          window.focus();
-          }
+      let params = randomizeParams();
+      
+      const words = ["word 1 ","word2"]
+      //const words = ["island boys kissing","isis application form","penis enlargment pills","pornhub gay","free v bucks generators","why is age of consent so old here","free gay porn","little boys kissing","gay hentai","massive gay orgy","fortnite sus emotes to do","thicc chun li hips","chun li doing party hips 10 hours loop"]
+        
+      
+      let google = "https://www.google.com/search?surl=1&sca_esv=3ec098e3b7112be9&rlz=1CAKLUN_enUS1139&q=" +  words[Math.floor(Math.random() * words.length)] + "&udm=2";
+      
+      let newWindow = window.open(google, 'test', params);
+      
+      if (newWindow) {
+        newWindow.blur();
+        window.focus();
+      } else {
+        console.log('Popup blocked or failed to open.');
+      }
+      
       setTimeout(loop, 125);
     }
   }
-
+  
   loop();
 }
