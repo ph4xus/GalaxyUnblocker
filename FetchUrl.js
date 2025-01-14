9aad27ce73e514f72d4de29043ca9fffdf3c8ecb
ph4xus <168146698+ph4xus@users.noreply.github.com>
2025-01-14 11:52:53 -0700

    Update FetchUrl.js

--- FetchUrl.js
+++ FetchUrl.js
@@ -1,1 +1,6 @@
-
+function spam() {
+  let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
+  width=0,height=0,left=-1000,top=-1000`;
+  
+  open('/', 'test', params);
+}
